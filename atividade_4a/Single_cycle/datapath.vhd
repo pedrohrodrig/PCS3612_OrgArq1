64b62dfd9f1be8475c9c1a72b2c583ccfb9c84c6
Pedro Viveiros <pedrohr.viveiros@gmail.com>
2022-11-22 23:10:25 -0300

    continuação da adição dos registradores de pipeline e mudança para std_logic

--- atividade_4a/Single_cycle/datapath.vhd
+++ atividade_4a/Single_cycle/datapath.vhd
@@ -1,35 +1,36 @@
 --Fluxo de dados
 library ieee;
-use ieee.numeric_bit.all;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 use ieee.math_real.ceil;
 use ieee.math_real.log2;
 
 entity datapath is
     port(
         -- Common
-        clock : in bit;
-        reset : in bit;
+        clock : in std_logic;
+        reset : in std_logic;
 
         -- From Control Unit
-        reg2loc  : in bit;
-        pcsrc    : in bit;
-        memToReg : in bit;
-        aluCtrl  : in bit_vector(3 downto 0);
-        aluSrc   : in bit;
-        regWrite : in bit;
+        reg2loc  : in std_logic;
+        pcsrc    : in std_logic;
+        memToReg : in std_logic;
+        aluCtrl  : in std_logic_vector(3 downto 0);
+        aluSrc   : in std_logic;
+        regWrite : in std_logic;
 
         -- To Control Unit
-        opcode : out bit_vector(10 downto 0);
-        zero   : out bit;
+        opcode : out std_logic_vector(10 downto 0);
+        zero   : out std_logic;
 
         -- IM Interface
-        imAddr : out bit_vector(63 downto 0);
-        imOut  : in bit_vector(31 downto 0); --Instrução coletada da memória de intruções
+        imAddr : out std_logic_vector(63 downto 0);
+        imOut  : in std_logic_vector(31 downto 0); --Instrução coletada da memória de intruções
 
         -- DM Interface
-        dmAddr : out bit_vector(63 downto 0);
-        dmIn   : out bit_vector(63 downto 0);
-        dmOut  : in bit_vector(63 downto 0)
+        dmAddr : out std_logic_vector(63 downto 0);
+        dmIn   : out std_logic_vector(63 downto 0);
+        dmOut  : in std_logic_vector(63 downto 0)
     );
 end entity datapath;
 
@@ -40,12 +41,12 @@ architecture PoliLeg_FD of datapath is
             size : natural := 64
         );
         port(
-            A, B : in  bit_vector(size-1 downto 0); -- inputs
-            F    : out bit_vector(size-1 downto 0); -- output
-            S    : in  bit_vector(3 downto 0); -- op selection
-            Z    : out bit; -- zero flag
-            Ov   : out bit; -- overflow flag
-            Co   : out bit -- carry out
+            A, B : in  std_logic_vector(size-1 downto 0); -- inputs
+            F    : out std_logic_vector(size-1 downto 0); -- output
+            S    : in  std_logic_vector(3 downto 0); -- op selection
+            Z    : out std_logic; -- zero flag
+            Ov   : out std_logic; -- overflow flag
+            Co   : out std_logic -- carry out
         );
     end component;
 
@@ -55,19 +56,19 @@ architecture PoliLeg_FD of datapath is
             wordSize : natural := 64
         );
         port(
-            clock        : in bit;
-            reset        : in bit;
-            regWrite     : in bit;
-            rr1, rr2, wr : in bit_vector(natural(ceil(log2(real(regn))))-1 downto 0); -- bit_vector(log2(regn)-1 downto 0)
-            d            : in bit_vector(wordSize-1 downto 0);
-            q1, q2       : out bit_vector(wordSize-1 downto 0)
+            clock        : in std_logic;
+            reset        : in std_logic;
+            regWrite     : in std_logic;
+            rr1, rr2, wr : in std_logic_vector(natural(ceil(log2(real(regn))))-1 downto 0); -- std_logic_vector(log2(regn)-1 downto 0)
+            d            : in std_logic_vector(wordSize-1 downto 0);
+            q1, q2       : out std_logic_vector(wordSize-1 downto 0)
         );
     end component;
 
     component signExtend is
         port(
-            i: in  bit_vector(31 downto 0); -- input
-            o: out bit_vector(63 downto 0) -- output
+            i: in  std_logic_vector(31 downto 0); -- input
+            o: out std_logic_vector(63 downto 0) -- output
         );
     end component;
 
@@ -76,27 +77,28 @@ architecture PoliLeg_FD of datapath is
             word_size: positive := 4
         );
         port (
-            clock, clear, set, enable : in bit;
-            control                   : in bit_vector(1 downto 0);
-            serial_input              : in bit;
-            parallel_input            : in bit_vector(word_size-1 downto 0);
-            parallel_output           : out bit_vector(word_size-1 downto 0)
+            clock, clear, set, enable : in std_logic;
+            control                   : in std_logic_vector(1 downto 0);
+            serial_input              : in std_logic;
+            parallel_input            : in std_logic_vector(word_size-1 downto 0);
+            parallel_output           : out std_logic_vector(word_size-1 downto 0)
         );
     end component;
 
-    signal PCNextOrdInst, PCNextInst, PCBranchInst : bit_vector(63 downto 0);
-    signal imD1, imD2                              : bit_vector(63 downto 0);
-    signal dmToIm                                  : bit_vector(63 downto 0);
-    signal MUXImOut                                : bit_vector(4 downto 0);
-    signal imD2OrExtAddr                           : bit_vector(63 downto 0);
-    signal dmAddr_o, imAddr_o, dmIn_o              : bit_vector(63 downto 0);
-    signal imOut_o                                 : bit_vector(31 downto 0);
-    signal dmOut_o                                 : bit_vector(63 downto 0);
-    signal extAddr, shiftedExtAddr                 : bit_vector(63 downto 0);
-    signal out_IFID, in_IFID                       : bit_vector(63 downto 0);
-    signal out_IDEX, in_IDEX                       : bit_vector(63 downto 0);
-    signal out_EXMEM, in_EXMEM                     : bit_vector(63 downto 0);
-    signal out_MEMWB, in_MEMWB                     : bit_vector(63 downto 0);
+    signal PCNextOrdInst, PCNextInst, PCBranchInst : std_logic_vector(63 downto 0);
+    signal imD1, imD2                              : std_logic_vector(63 downto 0);
+    signal dmToIm                                  : std_logic_vector(63 downto 0);
+    signal MUXImOut                                : std_logic_vector(4 downto 0);
+    signal imD2OrExtAddr                           : std_logic_vector(63 downto 0);
+    signal dmAddr_o, imAddr_o, dmIn_o              : std_logic_vector(63 downto 0);
+    signal imOut_o                                 : std_logic_vector(31 downto 0);
+    signal dmOut_o                                 : std_logic_vector(63 downto 0);
+    signal extAddr, shiftedExtAddr                 : std_logic_vector(63 downto 0);
+    signal out_IFID, in_IFID                       : std_logic_vector(63 downto 0);
+    signal out_IDEX, in_IDEX                       : std_logic_vector(63 downto 0);
+    signal out_EXMEM, in_EXMEM                     : std_logic_vector(63 downto 0);
+    signal out_MEMWB, in_MEMWB                     : std_logic_vector(63 downto 0);
+    signal not_clock                               : std_logic;
 
 begin
 
@@ -120,18 +122,20 @@ begin
     
     --Registradores do pipeline
 
+    not_clock <= not clock;
+
     --registrador IF/ID
-    IfId: registrador_universal
-        generic map(64)
+    REG_IF_ID: registrador_universal
+        generic map(word_size => 64)
         port map(
-            clock, 
-            reset, 
-            '0', 
-            '1',
-            "11",
-            '0',
-            in_IFID,
-            out_IFID
+            clock           => clock, 
+            clear           => reset, 
+            set             => '0', 
+            enable          => '1',
+            control         => "11",
+            serial_input    => '0',
+            parallel_input  => in_IFID,
+            parallel_output => out_IFID
         );
     
     --registrador ID/EX
@@ -141,17 +145,17 @@ begin
             --Entrada do somador (imm)
             --Entrada do mux (imm)
             --Entrada do somador (PCatual)
-    IdEx: registrador_universal
-        generic map(256)
+    REG_ID_EX: registrador_universal
+        generic map(word_size => 256)
         port map(
-            clock, 
-            reset, 
-            '0', 
-            '1',
-            "11",
-            '0',
-            in_IDEX,
-            out_IDEX
+            clock           => clock, 
+            clear           => reset, 
+            set             => '0', 
+            enable          => '1',
+            control         => "11",
+            serial_input    => '0',
+            parallel_input  => in_IDEX,
+            parallel_output => out_IDEX
         );
 
     --registrador EX/MEM
@@ -165,17 +169,17 @@ begin
             --Zero 
             --Endereço da memória de dados
             --Write data
-    ExMEM: registrador_universal
-        generic map(64)
+    REG_EX_MEM: registrador_universal
+        generic map(word_size => 64)
         port map(
-            clock, 
-            reset, 
-            '0', 
-            '1',
-            "11",
-            '0',
-            in_EXMEM,
-            out_EXMEM
+            clock           => clock, 
+            clear           => reset, 
+            set             => '0', 
+            enable          => '1',
+            control         => "11",
+            serial_input    => '0',
+            parallel_input  => in_EXMEM,
+            parallel_output => out_EXMEM
         );
 
     --registrador MEM/WB
@@ -185,17 +189,17 @@ begin
     --Saídas:
             --Ambas as entradas num MUX que alimenta o Write Data
 
-    MEMWb: registrador_universal
-        generic map(64)
+    REG_MEM_WB: registrador_universal
+        generic map(word_size => 64)
         port map(
-            clock, 
-            reset, 
-            '0', 
-            '1',
-            "11",
-            '0',
-            in_MEMWB,
-            out_MEMWB
+            clock           => clock, 
+            clear           => reset, 
+            set             => '0', 
+            enable          => '1',
+            control         => "11",
+            serial_input    => '0',
+            parallel_input  => in_MEMWB,
+            parallel_output => out_MEMWB
         );
 
     -- somador da proxima instrucao ordenada
@@ -238,7 +242,7 @@ begin
             64
         )
         port map(
-            clock,
+            not_clock,
             reset,
             regWrite,
             imOut_o(9 downto 5), --rr1
